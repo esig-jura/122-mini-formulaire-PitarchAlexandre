e1ef282009fa3c74e377d2ddb5b03b4cf8a3b40d
PitarchAlexandre <alexandre.pitarch@divcom.ch>
2024-04-17 15:26:47 +0200

    ajout eventListener

--- js/main.js
+++ js/main.js
@@ -5,4 +5,12 @@
 
 'use strict';
 
-console.log('Test liaison JS');
+//Récupération du formulaire
+const formulaire = document.querySelector('form');
+console.log(formulaire);
+
+//Ecouter l'envoi du formulaire
+formulaire.addEventListener('submit', (e) => {
+    alert('Coucoucoucoucoucoucoucou');
+})
+
