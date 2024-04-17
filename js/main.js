9328df99cf671a3303b0c2ae56f86c9bd4628e1f
PitarchAlexandre <alexandre.pitarch@divcom.ch>
2024-04-17 16:00:30 +0200

    tableau d'erreurs + if et for of qui affiche les erreurs

--- js/main.js
+++ js/main.js
@@ -15,19 +15,35 @@ console.log(formulaire, txtMDP, txtMDPCConfirm );
 formulaire.addEventListener('submit', (event) =>  {
     // Stoppe l'envoie du formulaire
     event.preventDefault();
-
+    let erreurs = [];
     // Récupérer la valeur du mot de passe
     let mdp = txtMDP.value;
     let mdpConfirm  = txtMDPCConfirm.value;
 
+    // Tester si mdp > 3
+    if (txtMDP.value.length < 3) {
+        erreurs.push('Le mot de passe doit contenir au moins 3 caractères !');
+    }
+
     // Tester si les mdp sont identiques
     if(mdpConfirm !== mdp) {
-        alert('Les mots de passes sont différents !!!');
-
-    } else {
-        alert('Compte créé avec succès !');
-        // Permet d'envoyer le formulaire
-        formulaire.submit();
+        erreurs.push('Les mots de passes sont différents !!!');
     }
-});
 
+    // S'il y a des erreurs
+    if (erreurs.length > 0) {
+        let msg = '';
+        for (let e of erreurs) {
+            msg += ' \n - ' + e;
+        }
+        alert(msg);
+        // Stoppe la fonction
+        return;
+    }
+
+
+    alert('Compte créé avec succès !');
+    // Permet d'envoyer le formulaire
+    formulaire.submit();
+
+});
